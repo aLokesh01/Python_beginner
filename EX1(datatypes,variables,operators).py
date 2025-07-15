888d468831f967299125d499bda7db1757e46ecb
aLokesh01 <lokesh.ajw@gmail.com>
2025-07-15 11:03:08 +0530

    Add files via upload

---
+++ EX1(datatypes,variables,operators).py
@@ -0,0 +1,21 @@
+integer_var=10
+float_var=3.14
+string_var="hello,python"
+boolean_var=True
+sum_result=integer_var+float_var
+product_result=integer_var*float_var
+is_equal=(integer_var==10)
+is_greater=(integer_var>5)
+logical_and=(is_equal and is_greater)
+logical_or=(is_equal or is_greater)
+print(integer_var)
+print(integer_var)
+print(float_var)
+print(string_var)
+print(boolean_var)
+print(sum_result)
+print(product_result)
+print(is_equal)
+print(is_greater)
+print(logical_and)
+print(logical_or)
