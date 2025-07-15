ab7e12e7b73e023643d0e3ff28d5acbeef36eb7f
aLokesh01 <lokesh.ajw@gmail.com>
2025-07-15 11:46:28 +0530

    Add files via upload

---
+++ UPDATED_EX2(tuple,dict,membership,identity).py
@@ -0,0 +1,38 @@
+integer_var = 10
+float_var = 3.14
+string_var = "hello,python|"
+boolean_var = True
+my_list = [1, 2, 3, 4]
+my_tuple = (5, 6, 7)
+my_dict = {"a": 1, "b": 2}
+my_set = {8, 9, 10}
+sum_result = integer_var + float_var
+product_result = integer_var * float_var
+is_equal = (integer_var == 10)
+is_greater = (integer_var > 5)
+logical_and = (is_equal and is_greater)
+logical_or = (is_equal or boolean_var)
+list_membership = 3 in my_list
+tuple_membership = 7 in my_tuple
+set_membership = 10 in my_set
+dict_membership = "a" in my_dict
+identity_operator = integer_var is 10
+print("integer_var =", integer_var)
+print("float_var =", float_var)
+print("string_var =", string_var)
+print("boolean_var =", boolean_var)
+print("integer variable:", integer_var)
+print("float variable:", float_var)
+print("string variable:", string_var)
+print("boolean variable:", boolean_var)
+print("sum of integers and float:", sum_result)
+print("product of integers and float:", product_result)
+print("is integer variable equal to 10?", is_equal)
+print("is integer variable greater than 5?", is_greater)
+print("Logical AND result:", logical_and)
+print("Logical OR result:", logical_or)
+print("Is 3 in the list?", list_membership)
+print("Is 7 in the tuple?", tuple_membership)
+print("Is 10 in the set?", set_membership)
+print("Is 'a' in the dictionary?", dict_membership)
+print("Is integer_var 10?", identity_operator)
